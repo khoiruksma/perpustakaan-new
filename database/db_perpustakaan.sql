a4d743b7856b66dc47463e0700ce305076f6f6c1
khoiruksma <khoirukusuma03@gmail.com>
2025-06-01 19:52:15 +0700

    first commit

---
+++ database/db_perpustakaan.sql
@@ -0,0 +1,326 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Sep 2023 pada 01.32
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(125) NOT NULL,
+  `kategori_buku` varchar(125) NOT NULL,
+  `penerbit_buku` varchar(125) NOT NULL,
+  `pengarang` varchar(125) NOT NULL,
+  `tahun_terbit` varchar(125) NOT NULL,
+  `isbn` int(50) NOT NULL,
+  `j_buku_baik` varchar(125) NOT NULL,
+  `j_buku_rusak` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `kategori_buku`, `penerbit_buku`, `pengarang`, `tahun_terbit`, `isbn`, `j_buku_baik`, `j_buku_rusak`) VALUES
+(1, 'Cara membuat tabel Microsoft Word', 'Perkantoran', 'Ganessa Exac', 'Andi', '2003', 2147483647, '10', '0'),
+(2, 'Kimia Utama', 'Kimia', 'Tirtamedia', 'Eko', '2013', 2147483647, '100', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `identitas`
+--
+
+CREATE TABLE `identitas` (
+  `id_identitas` int(11) NOT NULL,
+  `nama_app` varchar(50) NOT NULL,
+  `alamat_app` text NOT NULL,
+  `email_app` varchar(125) NOT NULL,
+  `nomor_hp` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `identitas`
+--
+
+INSERT INTO `identitas` (`id_identitas`, `nama_app`, `alamat_app`, `email_app`, `nomor_hp`) VALUES
+(1, 'E - Perpus', 'JL. Jogja Wates Kulon Progo', 'contact@e-perpus.com', '022487557789');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kode_kategori` varchar(50) NOT NULL,
+  `nama_kategori` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `kode_kategori`, `nama_kategori`) VALUES
+(1, 'KT-001', 'Perkantoran'),
+(2, 'KT-002', 'Kimia'),
+(3, 'KT-003', 'Biologi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemberitahuan`
+--
+
+CREATE TABLE `pemberitahuan` (
+  `id_pemberitahuan` int(11) NOT NULL,
+  `isi_pemberitahuan` varchar(255) NOT NULL,
+  `level_user` varchar(125) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pemberitahuan`
+--
+
+INSERT INTO `pemberitahuan` (`id_pemberitahuan`, `isi_pemberitahuan`, `level_user`, `status`) VALUES
+(1, '<i class=\'fa fa-exchange\'></i> #Riani Telah meminjam Buku', 'Admin', 'Sudah dibaca'),
+(2, '<i class=\'fa fa-repeat\'></i> #Riani Telah mengembalikan Buku', 'Admin', 'Sudah dibaca'),
+(3, '<i class=\'fa fa-exchange\'></i> #Joni Riyanto Telah meminjam Buku', 'Admin', 'Belum dibaca');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `nama_anggota` varchar(125) NOT NULL,
+  `judul_buku` varchar(125) NOT NULL,
+  `tanggal_peminjaman` varchar(125) NOT NULL,
+  `tanggal_pengembalian` varchar(50) NOT NULL,
+  `kondisi_buku_saat_dipinjam` varchar(125) NOT NULL,
+  `kondisi_buku_saat_dikembalikan` varchar(125) NOT NULL,
+  `denda` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `nama_anggota`, `judul_buku`, `tanggal_peminjaman`, `tanggal_pengembalian`, `kondisi_buku_saat_dipinjam`, `kondisi_buku_saat_dikembalikan`, `denda`) VALUES
+(1, 'Riani', 'Cara membuat tabel Microsoft Word', '13-09-2023', '14-09-2023', 'Baik', 'Baik', 'Tidak ada'),
+(2, 'Joni Riyanto', 'Kimia Utama', '14-09-2023', '', 'Baik', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `kode_penerbit` varchar(125) NOT NULL,
+  `nama_penerbit` varchar(50) NOT NULL,
+  `verif_penerbit` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `kode_penerbit`, `nama_penerbit`, `verif_penerbit`) VALUES
+(1, 'P001', 'Ganessa Exac', 'Terverifikasi'),
+(2, 'P002', 'Tirtamedia', 'Terverifikasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id_pesan` int(11) NOT NULL,
+  `penerima` varchar(50) NOT NULL,
+  `pengirim` varchar(50) NOT NULL,
+  `judul_pesan` varchar(50) NOT NULL,
+  `isi_pesan` text NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `tanggal_kirim` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pesan`
+--
+
+INSERT INTO `pesan` (`id_pesan`, `penerima`, `pengirim`, `judul_pesan`, `isi_pesan`, `status`, `tanggal_kirim`) VALUES
+(1, 'Riani', 'Ria', 'cara membuat tabel microsoft words', 'Jangan lupa untuk dikembalikan tanggal 14-09-2023', 'Sudah dibaca', '14-09-2023'),
+(2, 'Administrator', 'Riani', 'cara membuat tabel microsoft words', 'Baik', 'Sudah dibaca', '14-09-2023'),
+(3, 'Riani', 'Administrator', 'cara membuat tabel microsoft words', 'Tolong kembalikan untuk bukunya tangaal 14-9-2023', 'Belum dibaca', '14-09-2023'),
+(4, 'Joni Riyanto', 'Administrator', 'kimia', 'Tolong dikembalikan tanggal 14-09-2023', 'Sudah dibaca', '14-09-2023');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `kode_user` varchar(25) NOT NULL,
+  `nis` char(20) NOT NULL,
+  `fullname` varchar(125) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `kelas` varchar(50) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `verif` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `join_date` varchar(125) NOT NULL,
+  `terakhir_login` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `kode_user`, `nis`, `fullname`, `username`, `password`, `kelas`, `alamat`, `verif`, `role`, `join_date`, `terakhir_login`) VALUES
+(1, '-', '-', 'Administrator', 'admin', 'admin', '-', '-', 'Iya', 'Admin', '04-05-2021', '14-09-2023 ( 06:28:02 )'),
+(2, 'AP001', '12345678', 'Ria', 'ria', 'anggota', 'X - Administrasi Perkantoran', 'Jalan baru yogyakarta', 'Tidak', 'Anggota', '13-09-2023', ''),
+(3, '-', '-', 'Ria', 'anggota', 'anggota', '-', '-', 'Iya', 'Admin', '13-09-2023', '14-09-2023 ( 04:56:54 )'),
+(4, 'AP002', '123434', 'Eko', 'eko', 'eko', 'X - Farmasi', 'jalan bambu', 'Tidak', 'Anggota', '13-09-2023', ''),
+(6, 'AP003', '', 'Riani', 'riani', 'riani', '', '', 'Tidak', 'Anggota', '13-09-2023', '14-09-2023 ( 06:02:32 )'),
+(7, 'AP004', '', 'Joni Riyanto', 'joni', 'joni', '', '', 'Tidak', 'Anggota', '14-09-2023', '14-09-2023 ( 06:08:23 )');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `identitas`
+--
+ALTER TABLE `identitas`
+  ADD PRIMARY KEY (`id_identitas`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `pemberitahuan`
+--
+ALTER TABLE `pemberitahuan`
+  ADD PRIMARY KEY (`id_pemberitahuan`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indeks untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `identitas`
+--
+ALTER TABLE `identitas`
+  MODIFY `id_identitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemberitahuan`
+--
+ALTER TABLE `pemberitahuan`
+  MODIFY `id_pemberitahuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
